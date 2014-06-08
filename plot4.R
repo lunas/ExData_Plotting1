c3bbbb42175b024ea75f3ee2f2a881a18bf254c3
lunas <lukasnick@gmail.com>
2014-06-08 18:00:33 +0200

    removed one title from plot4

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ par(mfrow = c(2, 2))
 plot(as.POSIXct(df$datetime), df$Global_active_power, t="l", ylab="Global Active Power", xlab="")    
 plot(as.POSIXct(df$datetime), df$Voltage, t="l", ylab="Voltage", xlab="date+time") 
 
-plot(as.POSIXct(df$datetime), df$Sub_metering_1, t="l", main="Energy sub metering", ylab="Energy sub metering", xlab="")
+plot(as.POSIXct(df$datetime), df$Sub_metering_1, t="l", ylab="Energy sub metering", xlab="")
 lines(as.POSIXct(df$datetime), df$Sub_metering_2, col="red")
 lines(as.POSIXct(df$datetime), df$Sub_metering_3, col="blue")
 legend("topright", lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
